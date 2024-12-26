bb04f09cc51e5279fb5e0d14c07cc55440b921e5
Roope Hongisto <roope@Roopes-MacBook-Air.local>
2024-12-26 16:42:47 +0200

    main

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	filename := os.Args[1]
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("Error reading file:", err)
+	}
+	// parse input
+	graph, ants, err := ParseInput(string(data))
+	if err != nil {
+		log.Fatalln("Error parsing input:", err)
+		return
+	}
+	// // find shortest path
+	// paths := findpaths(graph)
+
+	// // simulate ants movement
+	// simulateants(ants, paths)
+
+	// // output result
+	// outputResults()
+}
